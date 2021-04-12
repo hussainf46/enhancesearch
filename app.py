9812f6c02e5608d0f5b2f10fc6e51ecd249059cc
hussainf46 <49541727+hussainf46@users.noreply.github.com>
2021-04-12 13:48:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,15 +46,14 @@ def predict():
     For rendering results on HTML GUI
     '''
     result=db.questions.find()
-    print(result) 
-    print('hello')
+    #print(result) 
+    #print('hello')
     for q in result:#{"_id":{"$oid":"5fa2aa3c7540c00017dc0b90"},"topics":[{"$oid":"5f77483d15314e0f04a9e7c1"}],"votes":{"$numberInt":"2"},"reportStatus":{"$numberInt":"0"},"user":{"$oid":"5fa2a7c57540c00017dc0b8f"},"question":"Why am I so afraid of failure?","description":"People are afraid of failure for a different kind of reason. I have had experienced fear of failure before and one of the reason for me was that I was to much concerned about what people would think of me after my failure. Also I didn’t want to disappoint people whose opinion I value. But then I realize that it is ok to fail and everybody fail once in a while. Failing has taught me a lot of thing about life. The main point is to get back up again after you fail!","time":{"$date":{"$numberLong":"1604495932357"}},"__v":{"$numberInt":"0"},"requestStatus":{"$numberInt":"0"}}
-        print("hiii")
+        #print("hiii")
         #print(q['_id'])
         #print(q['question'])
         fi.append({'id': q['_id'],'question' : q['question']})
-    for obj in fi:
-        print(obj['id'] , obj['question'],sep=' ')
+        
     data=request.get_json()
     
     output=[]
@@ -64,7 +63,7 @@ def predict():
     for tu in fi:
         sent2=tu['question']
         search_doc=nlp(sent2)
-        print(sent2)
+        #print(sent2)
         
         search_doc_no_stop_words = nlp(' '.join([str(t) for t in search_doc if not t.is_stop]))
         main_doc_no_stop_words = nlp(' '.join([str(t) for t in main_doc if not t.is_stop]))
